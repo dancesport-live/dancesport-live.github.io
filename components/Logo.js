608eaf8d3165139391a7a73cfb1f6bcb5ae94b7e
Niels Hoppe <mail@nielshoppe.de>
2024-11-13 13:54:11 +0100

    Update home page

---
+++ components/Logo.js
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <span className="text-3xl">
+      <span className="font-bold">Dance</span>
+      <span>Sport</span>
+      <span className="text-2xl bg-red-700 p-0.5 pl-1 pr-1.5 ml-1 rounded-tl-lg rounded-br-lg">.live</span>
+    </span>
+  )
+}
